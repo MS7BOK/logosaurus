c5ad09a2304ade53ec0a04c108024dcd148f069c
MS7BOK <maxsebok04@gmail.com>
2023-10-30 15:16:03 +1100

    added code/functions to build the logos and set the parameters colour shapec etc.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const Circle = require('./lib/Circle');
+const Square = require('./lib/Square');
+const Triangle = require('./lib/Triangle');
+
+async function generateLogo() {
+  try {
+    const userInput = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo text:',
+        validate: function(input) {
+          return input.length > 0 && input.length <= 3 ? true : 'Please enter 1 to 3 characters.';
+        }
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter text color (keyword or hexadecimal number):'
+      },
+      {
+        type: 'list',
+        name: 'shapeType',
+        message: 'Choose a shape:',
+        choices: ['circle', 'square', 'triangle']
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color (keyword or hexadecimal number):'
+      }
+    ]);
+
+    let shape;
+    switch (userInput.shapeType) {
+      case 'circle':
+        shape = new Circle(50, userInput.shapeColor); // Adjust the radius as needed
+        break;
+      case 'square':
+        shape = new Square(50, userInput.shapeColor); // Adjust the side length as needed
+        break;
+      case 'triangle':
+        shape = new Triangle(100, 100, userInput.shapeColor); // Adjust the base and height as needed
+        break;
+      default:
+        console.error('Invalid shape type');
+        return;
+    }
+
+    const svgContent = shape.render();
+
+    fs.writeFileSync('logo.svg', svgContent);
+
+    console.log('Generated logo.svg');
+  } catch (error) {
+    console.error('Error generating logo:', error);
+  }
+}
+
+generateLogo();
